0fb67f919236b309f68e349e5eff0a8b079d7b07
wenwan <aalansehaiyang52@126.com>
2017-06-20 12:43:47 +0800

    使用@Async实现异步调用

--- Chapter4-1-2/src/main/java/com/didispace/Application.java
+++ Chapter4-1-2/src/main/java/com/didispace/Application.java
@@ -3,7 +3,6 @@ package com.didispace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
